40146d5c3a015c98cdc48d4e50a36ec9ce9afba4
Darren <darrenhay1994@gmail.com>
2018-02-02 23:06:37 +0200

    removing bower and upgrading create-react-app to new version

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { RIEToggle, RIEInput, RIETextArea, RIENumber, RIETags, RIESelect } from 'riek';
-import '../bower_components/bootstrap/dist/css/bootstrap.min.css'
-import '../bower_components/jquery/dist/jquery.min.js'
 import './index.css';
 
 
